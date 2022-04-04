5633ce4d66776e57337848f208af64541230213e
Ellen Considine <ellen_considine@g.harvard.edu>
2022-04-04 11:33:59 -0400

    Update Pie_charts.R

--- Pie_charts.R
+++ Pie_charts.R
@@ -1,16 +1,50 @@
 library(ggplot2)
+library(cowplot)
+library(stringr)
 
-data1<-data.frame(state=c("AZ", "CA", "CO", "ID", "MT", "WY", "NM", "OR", "UT", "WA"), number = summary(as.factor(valid$stateCode)) )
-ggplot(data1, aes(x="", y=number, fill=state)) +
-  geom_bar(stat="identity", width=1) +
-  coord_polar("y", start=0)
+setwd("C:/Users/ellen/OneDrive/MyDocs/Graduate Research/Wildfire data project")
+valid<- readRDS("Validation_set.rds")
+valid$stateCode<- as.factor(valid$stateCode)
+valid$season<- str_to_title(valid$season)
+valid$season<- as.factor(valid$season)
 
-data2<-data.frame(season=c('spring','summer','autumn','winter'),number=summary(as.factor(valid$season)))
-ggplot(data2, aes(x="", y=number, fill=season)) +
-  geom_bar(stat="identity", width=1) +
-  coord_polar("y", start=0)
+data1<-data.frame(State=levels(valid$stateCode), number = summary(valid$stateCode) )
+p1<- ggplot(data1, aes(x="", y=number, fill=State)) +
+  geom_bar(stat="identity", width=1) + ggtitle("Observations per State") +
+  coord_polar("y", start=0) + xlab("") + ylab("") + theme(axis.text = element_blank(),
+                                                          axis.ticks = element_blank(),
+                                                          panel.grid  = element_blank())
+leg1<- get_legend(p1 + theme(legend.position = "bottom", legend.box = "vertical",
+                          legend.key.width = unit(0.25, "line"), 
+                          legend.spacing.y = unit(0.1, "cm"),
+                          legend.title = element_text(face="bold")))
 
-data3<-data.frame(year=c(2008:2016),number=summary(as.factor(valid$year)))
-ggplot(data3, aes(x="", y=number, fill=year)) +
-  geom_bar(stat="identity", width=1) +
-  coord_polar("y", start=0)
+data2<-data.frame(Season=levels(valid$season),number=summary(valid$season))
+p2<- ggplot(data2, aes(x="", y=number, fill=Season)) +
+  geom_bar(stat="identity", width=1) + ggtitle("Observations per Season") +
+  coord_polar("y", start=0) + xlab("") + ylab("") + theme(axis.text = element_blank(),
+                                                          axis.ticks = element_blank(),
+                                                          panel.grid  = element_blank())
+leg2<- get_legend(p2 + theme(legend.position = "bottom", legend.box = "vertical",
+                             legend.key.width = unit(0.25, "line"), 
+                             legend.spacing.y = unit(0.1, "cm"),
+                             legend.title = element_text(face="bold")))
+
+data3<-data.frame(Year=c(2008:2016),number=summary(as.factor(valid$year)))
+p3<- ggplot(data3, aes(x="", y=number, fill=Year)) +
+  geom_bar(stat="identity", width=1) + ggtitle("Observations per Year") +
+  coord_polar("y", start=0) + xlab("") + ylab("") + theme(axis.text = element_blank(),
+                                                          axis.ticks = element_blank(),
+                                                          panel.grid  = element_blank())
+leg3<- get_legend(p3 + theme(legend.position = "bottom", legend.box = "vertical",
+                             legend.key.width = unit(2.5, "line"), 
+                             legend.spacing.y = unit(0.1, "cm"),
+                             legend.title = element_text(face="bold")))
+
+plots<- plot_grid(p3 + theme(legend.position="none"), p2 + theme(legend.position="none"),
+                  p1 + theme(legend.position="none"), nrow=1)
+legends<- plot_grid(leg3, leg2, leg1, nrow=1)
+
+png("Plots/Pie_charts.png", width = 700, height = 250)
+plot_grid(plots, NULL, legends, nrow = 3, rel_heights = c(1, -0.25, 0.6))
+dev.off()
