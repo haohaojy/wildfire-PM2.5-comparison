643fd4a38081a161a8f4f5f85e86522f0a103b7b
EllenConsidine <ellen.considine@colorado.edu>
2022-05-26 13:34:24 -0400

    updated ST metrics and plots, tables 1 and 2

--- Summary-stats_validation-set.R
+++ Summary-stats_validation-set.R
@@ -1,31 +1,39 @@
 #### Generating Tables 1 and 2 (summary statistics of the validation set)
 
-setwd("C:/Users/ellen/OneDrive/MyDocs/Graduate Research/Wildfire data project")
-valid<- readRDS("Validation_set.rds")
+library(xtable)
+
+valid<- readRDS("Merged_monitor_Di_Reid_with_CMAQ.rds")
+
+di_na_pos<- which(is.na(valid$PM_Di))
 
 ## Make Table 1:
 
-Table.1<- cbind(summary(valid$PM2.5), summary(valid$PM_grid[-di_na_pos]), summary(valid$Ens_pred))
-Table.1<- rbind(Table.1, c(sd(valid$PM2.5), sd(valid$PM_grid[-di_na_pos]),
-                           sd(valid$Ens_pred)))
+Table.1<- cbind(summary(valid$PM2.5), summary(valid$PM_Di[-di_na_pos]), summary(valid$PM_Reid))
+Table.1<- rbind(Table.1, c(sd(valid$PM2.5), sd(valid$PM_Di[-di_na_pos]),
+                           sd(valid$PM_Reid)))
 
 Medium<- cbind(summary(valid[which(valid$PM2.5 >= 12.1),"PM2.5"]),
-               summary(valid[which(valid$PM2.5 >= 12.1),"PM_grid"])[-7], 
-               summary(valid[which(valid$PM2.5 >= 12.1),"Ens_pred"]))
+               summary(valid[which(valid$PM2.5 >= 12.1),"PM_Di"])[-7], 
+               summary(valid[which(valid$PM2.5 >= 12.1),"PM_Reid"]))
 Medium<- rbind(Medium, c(sd(valid[which(valid$PM2.5 >= 12.1),"PM2.5"]),
-                         sd(valid[which(valid$PM2.5 >= 12.1),"PM_grid"], na.rm = TRUE), 
-               sd(valid[which(valid$PM2.5 >= 12.1),"Ens_pred"])))
+                         sd(valid[which(valid$PM2.5 >= 12.1),"PM_Di"], na.rm = TRUE), 
+               sd(valid[which(valid$PM2.5 >= 12.1),"PM_Reid"])))
 
 High<- cbind(summary(valid[which(valid$PM2.5 >= 35.5),"PM2.5"]),
-               summary(valid[which(valid$PM2.5 >= 35.5),"PM_grid"])[-7], 
-               summary(valid[which(valid$PM2.5 >= 35.5),"Ens_pred"]))
+               summary(valid[which(valid$PM2.5 >= 35.5),"PM_Di"])[-7], 
+               summary(valid[which(valid$PM2.5 >= 35.5),"PM_Reid"]))
 High<- rbind(High, c(sd(valid[which(valid$PM2.5 >= 35.5),"PM2.5"]),
-                         sd(valid[which(valid$PM2.5 >= 35.5),"PM_grid"], na.rm = TRUE), 
-               sd(valid[which(valid$PM2.5 >= 35.5),"Ens_pred"])))
+                         sd(valid[which(valid$PM2.5 >= 35.5),"PM_Di"], na.rm = TRUE), 
+               sd(valid[which(valid$PM2.5 >= 35.5),"PM_Reid"])))
 
 Table.1<- cbind(Table.1, Medium, High)
 
 write.csv(round(Table.1,2), "Table_1.csv")
+
+## Print for Latex:
+
+xtable(Table.1)
+
 
 ## Make Table 2:
 
@@ -33,22 +41,22 @@ subsets<- function(variable, value){ # Example: "year", 2008
   pos<- which(valid[,variable] == value)
   N<- length(pos)
   
-  Means<- round(c(mean(valid[pos,"PM2.5"]), mean(valid[pos,"PM_grid"], na.rm=TRUE),
-            mean(valid[pos,"Ens_pred"]), 
+  Means<- round(c(mean(valid[pos,"PM2.5"]), mean(valid[pos,"PM_Di"], na.rm=TRUE),
+            mean(valid[pos,"PM_Reid"]), 
             mean(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"PM2.5"]),
-            mean(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"PM_grid"], na.rm=TRUE),
-            mean(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"Ens_pred"]),
+            mean(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"PM_Di"], na.rm=TRUE),
+            mean(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"PM_Reid"]),
             mean(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"PM2.5"]),
-            mean(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"PM_grid"], na.rm=TRUE),
-            mean(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"Ens_pred"])),2)
-  SDs<- round(c(sd(valid[pos,"PM2.5"]), sd(valid[pos,"PM_grid"], na.rm=TRUE),
-            sd(valid[pos,"Ens_pred"]),
+            mean(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"PM_Di"], na.rm=TRUE),
+            mean(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"PM_Reid"])),2)
+  SDs<- round(c(sd(valid[pos,"PM2.5"]), sd(valid[pos,"PM_Di"], na.rm=TRUE),
+            sd(valid[pos,"PM_Reid"]),
             sd(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"PM2.5"]),
-            sd(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"PM_grid"], na.rm=TRUE),
-            sd(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"Ens_pred"]),
+            sd(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"PM_Di"], na.rm=TRUE),
+            sd(valid[intersect(pos, which(valid$PM2.5 >= 12.1)),"PM_Reid"]),
             sd(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"PM2.5"]),
-            sd(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"PM_grid"], na.rm=TRUE),
-            sd(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"Ens_pred"])),2)
+            sd(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"PM_Di"], na.rm=TRUE),
+            sd(valid[intersect(pos, which(valid$PM2.5 >= 35.5)),"PM_Reid"])),2)
   SDs<- paste0(SDs, ")")
   return(c(paste0(value, " (", N, ", ", length(intersect(pos, which(valid$PM2.5 >= 12.1))),
                   ", ", length(intersect(pos, which(valid$PM2.5 >= 35.5))),
@@ -59,8 +67,14 @@ Seasons<- sapply(c("spring", "summer", "autumn", "winter"),
                  function(x) subsets("season", x))
 Years<- sapply(2008:2016, function(x) subsets("year", x))
 
+states<- sort(unique(valid$state))
 States<- sapply(states, function(x) subsets("state", x))
 
 Table.2<- rbind(t(Seasons), t(Years), t(States))
 
 write.csv(Table.2, "Table_2.csv")
+
+
+## Print for Latex:
+
+xtable(Table.2)
